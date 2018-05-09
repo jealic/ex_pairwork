77cc57ab3470d3bdaa275bccd8842a61ef06406a
Jessie Ling <jealic@hotmail.com>
2018-05-10 02:58:17 +0800

    add user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do |i|
+  user = User.create!(name: "user#{i+1}")
+  puts "#{user.name} created"
+end
